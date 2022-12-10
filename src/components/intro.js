022b990895c3605f77e3d719130cbde564522e39
Spencer Blanden <89828067+spencerblanden@users.noreply.github.com>
2022-12-10 08:55:38 -0600

    main: base site additions

---
+++ src/components/intro.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import "./component.css"
+
+const Intro = () => {
+
+    return (
+        <div class="intro">
+        <div className="greeting">Hi, my name is Spencer.</div>
+        <div className="mefact">This is a fun fact</div>
+        </div>
+    )
+    }
+export default Intro
+
