5118b3df8cbf1981144a64e1d7cee10fb16201f2
Spencer Blanden <89828067+spencerblanden@users.noreply.github.com>
2022-12-10 09:09:01 -0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Carousel from "../components/carousel"
 import Intro from "../components/intro"
-import Header from "../components/Header"
+import Header from "../components/header"
 
 const IndexPage = () => (
   <Layout>
